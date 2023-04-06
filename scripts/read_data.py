14395f91416a753857cb8c9ccfafc7d0cea4ccbd
Yago Ascasibar <yago.ascasibar@uam.es>
2023-04-06 15:07:13 +0800

    source and background power laws

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 from astropy.io import fits
+from astropy.wcs import WCS
 from time import time
 
 
@@ -17,40 +18,47 @@ def run(dataset):
     if dataset == 20:
         object_name = '2D Gaussian noise'
         data = np.random.normal(size=(1000, 1000)) * 42 + 666
+        wcs = None
 
     if dataset == 21:
         object_name = 'CIG 335'
         hdu = fits.open('data/CIG_335.fits')
         data = hdu[0].data[3000:4000, 1500:2500].astype(np.float32)
+        wcs = WCS(hdu[0].header)
 
     if dataset == 22:
         object_name = 'NGC2420'
         hdu = fits.open('data/uamA_0033.fits')
         data = hdu[0].data.astype(np.float32)
+        wcs = WCS(hdu[0].header)
 
     if dataset == 23:
         object_name = 'HGC 44 slice'
         hdu = fits.open('data/hcg44_cube_R.fits')
         data = hdu[0].data[69].astype(np.float32) # to make sure it's converted to float
+        wcs = WCS(hdu[0].header).celestial
 
     # 3D datacubes:
 
     if dataset == 30:
         object_name = '3D Gaussian noise'
         data = np.random.normal(size=(200, 200, 200)) * 42
+        wcs = None
 
     if dataset == 31:
         object_name = 'HGC 44'
         hdu = fits.open('data/hcg44_cube_R.fits')
         data = hdu[0].data[:, 150:350, 350:650].astype(np.float32)
+        wcs = WCS(hdu[0].header).celestial
 
     if dataset == 32:
         object_name = 'Synthetic WSRT cube'
         hdu = fits.open('data/sofiawsrtcube.fits')
         data = hdu[0].data.astype(np.float32)  # to make sure it's converted to float
+        wcs = WCS(hdu[0].header).celestial
 
     print(f"Read dataset {dataset}: \"{object_name}\" {data.shape} ({time()-t0:.3g} s)")
-    return object_name, data
+    return object_name, data, wcs
 
 
 if (__name__ == '__main__'):
